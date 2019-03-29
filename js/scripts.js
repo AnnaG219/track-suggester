8bf0a70d3d90622269eaf0a6a20d960e1cc5dff7
Anna Gibbons <anna@gibbons-clan.com>
2019-03-29 16:57:18 -0700

    added code to html, and to js, started suggester for groups.

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,66 @@
     <title>Survey!</title>
   </head>
   <body>
+  <div class="container">
+    <div>
+      <form id="formOne">
+
+        <div class="form-group">
+          <label for="person1">What is you name? (First, Last)</label>
+          <input id="person1" class="form-control" type="text">
+        </div>
+
+        <div class="form-group">
+          <label for="state">What State do you live in?</label>
+          <input id="state" class="form-control" type="text">
+        </div>
+      </form>
+    </di
+    <div class="radio">
+      <label>
+        <input type="radio" name="flavor" value="chocolate" checked>
+        Chocolate
+      </label>
+    </div>
+    <div class="radio">
+      <label>
+        <input type="radio" name="flavor" value="grape">
+        grape
+      </label>
+    </div>
+    <div class="radio">
+      <label>
+        <input type="radio" name="flavor" value="apple">
+        apple
+      </label>
+    </div>
+
+    <div class="radio">
+      <label>
+        <input type="radio" name="flavor" value="cherry" checked>
+        cherry
+      </label>
+    </div>
+    <div class="radio">
+      <label>
+        <input type="radio" name="flavor" value="banana">
+        banana
+      </label>
+    </div>
+    <div class="radio">
+      <label>
+        <input type="radio" name="flavor" value="orange">
+        orange
+      </label>
+    </div>
+    <div class="form-group">
+      <label for="born">Date of birth:</label>
+      <input id="born" class="form-control" type="date">
+    </div>
+    <div class="form-group">
+      <label for="color">What is your favorite color?</label>
+      <input id="color" class="form-control" type="color">
+    </div>
+  </div>
+</body>
+</html>
