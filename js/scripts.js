ae2c17348c8ddffb3b0f03c13b9092673da8d337
Anna Gibbons <anna@gibbons-clan.com>
2019-03-31 21:35:33 -0700

    debug the last of my js code so the quick will function with if elses.

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ $(document).ready(function() {
   $("form#Tracking_Suggester").submit(function(event) {
     event.preventDefault();
     var logicType = $("input[name=logicType]:checked").val();
-    var workType = $("input [name=workType]:checked").val();
-    var runTime = $("input [name=runTime]:checked").val();
+    var workType = $("input[name=workType]:checked").val();
+    var runTime = $("input[name=runTime]:checked").val();
     var enviroment = $("input[name=enviroment]:checked").val();
-    var company = $("input [name=company]:checked").val();
+    var company = $("input[name=company]:checked").val();
 
       if (logicType === "frontEnd") {
         $('#choice1').hide();
@@ -29,9 +29,16 @@ $(document).ready(function() {
         $('#choice5').hide();
 
       }
-      else if (logicType === "backEnd" && workType === "rest" && runTime === "compiled" && enviroment === "java") {
+      else if ( (logicType === "backEnd") && (workType === "rest")&& (runTime === "compiled") && (enviroment === "java") ) {
         $('#choice2').hide();
         $('#choice2').show();
+        $('#choice3').hide();
+        $('#choice4').hide();
+        $('#choice5').hide();
+      }
+      else if ( (logicType === "backEnd") && (workType === "rest")&& (runTime === "compiled") && (enviroment === "cSharp") ) {
+        $('#choice2').show();
+        $('#choice2').hide();
         $('#choice3').hide();
         $('#choice4').hide();
         $('#choice5').hide();
@@ -42,6 +49,7 @@ $(document).ready(function() {
         $('#choice3').hide();
         $('#choice4').hide();
         $('#choice5').hide();
+        $('#choice6').hide();
       }
   });
 });
